31be2d63769be6b25e1754f8f24ac5746cec319a
mangesh <mangesh>
2024-06-30 10:13:48 +0530

    sideDrawer

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,42 @@
+import BottomNavigator from "@/components/BottomNavigator";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Header from "@/components/Header";
-import BottomNavigator from "./screen/BottomNavigator";
-const Stack = createNativeStackNavigator();
+import LoanScreen from "./screen/LoanScreen";
+
+const Drawer = createDrawerNavigator();
+
+// Define an array of screen objects
+const screens = [
+  {
+    name: "Home",
+    component: BottomNavigator,
+    options: { drawerLabel: "Home", title: "Home Screen" },
+  },
+  {
+    name: "loan",
+    component: LoanScreen,
+    options: { drawerLabel: "loan", title: "loan" },
+  },
+];
 
 export default function Index() {
   return (
     <NavigationContainer independent={true}>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="BottomNavigator"
-          component={BottomNavigator}
-          options={{ header: () => <Header /> }}
-        />
-      </Stack.Navigator>
+      <Drawer.Navigator
+        screenOptions={{
+          drawerType: "slide",
+          overlayColor: "transparent",
+        }}
+      >
+        {screens.map((screen) => (
+          <Drawer.Screen
+            key={screen.name}
+            name={screen.name}
+            component={screen.component}
+            options={screen.options}
+          />
+        ))}
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
